216dc32e42bb9b1995c8b1699cae4c449f2b20a0
ashutosh-singh-03 <ashutosh5607singh@gmail.com>
2024-09-19 22:50:33 +0530

    First commit

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  MdAdminPanelSettings,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+  MdKeyboardDoubleArrowUp,
+} from "react-icons/md";
+import { LuClipboardEdit } from "react-icons/lu";
+import { FaNewspaper, FaUser } from "react-icons/fa";
+import { FaArrowsToDot } from "react-icons/fa6";
+import moment from "moment";
+
+const Dashboard = () => {
+  return <div className="h-full py-4">
+<div className="grid grid-cols-1 md:grid-cols-4 gap-5">
+
+</div>
+  </div>;
+};
+
+export default Dashboard;
