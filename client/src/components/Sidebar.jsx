589e65a0b155a61ed5380962748f70981f625664
ashutosh-singh-03 <ashutosh5607singh@gmail.com>
2024-09-20 02:52:03 +0530

    top

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import {
   MdDashboard,
   MdOutlineAddTask,
@@ -64,7 +65,6 @@ const Sidebar = () => {
     dispatch(setOpenSidebar(false));
   };
 
-  import PropTypes from 'prop-types';
 
   const NavLink = ({ el }) => {
     return (
